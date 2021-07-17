69fe091c6658cb8ba8941ed328c731ebe9ee76cd
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-07-17 09:35:11 -0400

    using the conditional statement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-let maleNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'kofi', 'Kwame'];
-let femaleNames=['Akosua', 'Adwoa','Abenaa', 'Akua', 'Afua', 'Ama'];
-let days=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
+let maleNames=['Kwame','Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'kofi'];
+let femaleNames=['Ama','Akosua', 'Adwoa','Abenaa', 'Akua', 'Yaa', 'Afua'];
+let days=['saturday', 'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
 
 function male(){
     let male=document.getElementById('male').value
@@ -17,6 +17,7 @@ function female(){
 }
 
 function akhanName(){
+    let main = document.getElementById('result').value
 
     let date = document.getElementById('birthday').value
     let year = String(date)
@@ -24,10 +25,18 @@ function akhanName(){
     let YY = parseInt(date.substr(2,2));
     let MM = parseInt(date.substr(5,2));
     let DD = parseInt(date.substr(8,2));
-    let theDay = (((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+    let theDay = Math.floor((((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+
+
+    let weekday = days[theDay]
+
+    if (weekday == 'sunday' &&  main == 'male') {
+    document.getElementById('result').innerHTML=(maleNames[1])
+    }
 
 
 
+    
 
     
 }
