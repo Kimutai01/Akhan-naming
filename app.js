6cd1df4483d13af49aa6ddaa10c6ea5582dae9b4
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-07-17 03:25:37 -0400

    substringing

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ let femaleNames=['Akosua', 'Adwoa','Abenaa', 'Akua', 'Afua', 'Ama'];
 let days=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
 
 function male(){
-    let name=document.getElementById('male').value
-    document.getElementById('result').innerHTML=male
+    let male=document.getElementById('male').value
+    // document.getElementById('result').innerHTML=male
+    alert(male)
 
 }
 
 function female(){
     let female=document.getElementById('female').value
-    document.getElementById('result').innerHTML=female
+    // document.getElementById('result').innerHTML=female
     alert(female)
+}
 
+function akhanName(){
+
+    let CC = dates.substr(0,2);
+    let YY = dates.substr(2,2);
+    let MM = dates.substr(5,2);
+    let DD = dates.substr(8,2);
+
+    
 }
