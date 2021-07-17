7ee38d09e4cf9113c574f22a8a73e46845bc779f
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-07-17 02:24:52 -0400

    declaring the male function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-document.getElementById('input-group').addEventListener('submit', akhanName);
 
+let maleNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'kofi', 'Kwame'];
+let femaleNames=['Akosua', 'Adwoa','Abenaa', 'Akua', 'Afua', 'Ama'];
+let days=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
 
-function akhanName(e){
-    console.log('akhan...')
+function male(){
+    let name=document.getElementById('male').value
+    alert(name);
+    document.getElementById('result').innerHTML=male
 
-
-    e.preventDefault();
 }
